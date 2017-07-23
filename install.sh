bbe5a96ea87f25937847958e42fda3cafcfc4f05
tim83 <tim.mees83g@gmail.com>
2017-07-23 18:19:02 +0200

    Install script

--- install.sh
+++ install.sh
@@ -5,6 +5,38 @@ PATH=~/.adapter
 mkdir -p $PATH
 cp -rv . $PATH
 
+os=$(cat /etc/os-release | grep ID | cut -d '=' -f2)
+if [ "$os" == "manjaro" ]
+	then
+	sudo pacman -S python3 python-pip python-wheel python-pyqt5	
+	sudo pip3 install os
+	sudo pip3 install sys
+	sudo pip3 install datetime
+	sudo pip3 install Pillow
+	sudo pip3 install matplotlib
+	sudo pip3 install PySerial
+	
+elif [ "$os" == "opensuse" ]
+	then
+	sudo zypper install python3 python3-pip python3-wheel python3-pyqt5
+	sudo pip3 install os
+	sudo pip3 install sys
+	sudo pip3 install datetime
+	sudo pip3 install Pillow
+	sudo pip3 install matplotlib
+	sudo pip3 install PySerial
+else
+	echo 'Distrubutie niet gevonden.'
+	echo 'Gelieven de volgende packatten te installeren voor python3:'
+	echo ' - datetime'
+	echo ' - os'
+	echo ' - sys'
+	echo ' - PyQt5'
+	echo ' - matplotlib'
+	echo ' - PySerial'
+	exit
+fi
+
 chmod +x $PATH/data.desktop $PATH/gui.desktop
 ln -sf $PATH/data.desktop ~/.config/autostart/battery-background.desktop
 ln -sf $PATH/gui.desktop $(xdg-user-dir DESKTOP)/battery-monitor.desktop
