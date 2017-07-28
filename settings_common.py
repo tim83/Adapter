ce5402ac5af716eb9bb5015ee5d842d5b4a1b677
tim83 <tim83@users.noreply.github.com>
2017-07-28 19:34:20 +0200

    Update settings_common.py

--- settings_common.py
+++ settings_common.py
@@ -7,7 +7,7 @@ IDLE = False
 #
 #	Data
 #
-INTERVAL = 10 #600 #tijd tussen updates (s)
+INTERVAL = 1 #600 #tijd tussen updates (s)
 PERIOD = 5 # periode die op de grafiek getoond wordt (h)
 #
 #	Arduino
