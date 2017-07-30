35bf1a493ae8bdf94898cd6a2ca2f44b3428fbbe
tim83 <tim.mees83g@gmail.com>
2017-07-30 13:43:42 +0200

    Edit

--- gui.py
+++ gui.py
@@ -24,7 +24,7 @@ def is_connected():
     try:
         # connect to the host -- tells us if the host is actually
         # reachable
-        socket.create_connection(("www.google.com", 80))
+        socket.create_connection(("www.github.com", 80))
         return True
     except OSError:
         pass
