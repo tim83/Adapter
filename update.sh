9d46eb01090b9270f49674ae32f96848d06e6c4e
tim83 <tim.mees83g@gmail.com>
2017-07-30 13:39:27 +0200

    Edit

--- update.sh
+++ update.sh
@@ -5,8 +5,9 @@ path=~/.adapter
 pwd=$PWD
 cd ~/.adapter/
 
-git pull
-#sed -i "s/\$HOME/$HOME/g" $path/gui.desktop
-#sed -i "s/\$HOME/$HOME/g" $path/data.desktop
+git fetch --all
+git reset --hard origin/master
+sed -i "s|\$HOME|$HOME|g" $path/gui.desktop
+sed -i "s|\$HOME|$HOME|g" $path/data.desktop
 
 cd $pwd
