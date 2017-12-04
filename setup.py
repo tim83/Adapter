dc3027cba9ec7e931c425095a3a25aa9afc896a9
Tim Mees <tim.mees83g@gmail.com>
2017-12-04 17:39:03 +0100

    Bokeh interface debuged

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import expanduser
 
 setup(
 	name='adapterctl',
-	version='0.44',
+	version='0.44.2',
 	packages=['adapterctl'],
 	url='https://github.com/tim83/adapterctl',
 	license='GNU General Public License',
