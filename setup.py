8c4c09d5085c0365c22e2e1d19da76f1798920f2
Tim Mees <tim.mees83g@gmail.com>
2020-06-04 14:35:32 +0200

    Edit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import expanduser
 
 setup(
 	name='adapterctl',
-	version='0.45.10',
+	version='0.45.11',
 	packages=['adapterctl'],
 	url='https://github.com/tim83/adapterctl',
 	license='GNU General Public License',
